3091d6d27dcc6490d49e917a8ced4280677a7007
abanoub899 <abanobgayed@outlook.com>
2021-08-02 21:39:00 +0200

    aba

--- dashboard/routes/web.php
+++ dashboard/routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/admin', function () {
+    return view('admin.dashboard');
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
